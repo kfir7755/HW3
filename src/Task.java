fccb74f92eafa475d800036be3aafb5a87852011
kfir7755 <kfir.eliyahu@campus.technion.ac.il>
2022-06-14 20:48:05 +0300

    added Task

--- src/Task.java
+++ src/Task.java
@@ -1,6 +1,37 @@
 import java.util.Date;
 
-public class Task {
-    private String description;
+public class Task implements Cloneable {
+    private final String description;
     private Date date;
+
+    public Task(String description, Date date) {
+        this.description = description;
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Task)){
+            return false;
+        }
+        if(this.hashCode()==obj.hashCode() && this.date.compareTo(((Task) obj).date)==0) {
+            return true;
+        }
+        else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.description.hashCode();
+    }
+
+    @Override
+    public String toString(){
+        return "("+this.description + "," + this.date + ")";
+    }
+
+    @Override
+    public Task clone(){
+        Task task= new Task(this.description, this.date);
+    }
 }
